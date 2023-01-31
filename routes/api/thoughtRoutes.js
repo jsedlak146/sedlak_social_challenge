69c3e1611050b457a67be828e96a26f47ac7055c
jsedlak146 <jssedlak146@gmail.com>
2023-01-31 14:38:30 -0500

    connects to server now

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,21 +1,21 @@
 const router = require('express').Router();
 const {
-  getThought,
   getSingleThought,
   createThought,
   deleteThought,
-  updateThought
+  updateThought,
+  getAllThoughts
 } = require('../../controllers/thoughtController');
 
 // /api/students
-router.route('/').get(getThought).post(createThought);
+router.route('/').get(getAllThoughts).post(createThought);
 
 // /api/students/:studentId
 router.route('/:thoughtID').get(getSingleThought).delete(deleteThought).put(updateThought);
 
 // /api/students/:studentId/assignments/:assignmentId
-router.route('/:thoughtID/reactions').post(addReaction);
+//router.route('/:thoughtID/reactions').//post(addReaction);
 
-router.route('/:thoughtID/reactions/:reactionID').delete(deleteReaction);
+//router.route('/:thoughtID/reactions/:reactionID').delete(deleteReaction);
 
 module.exports = router;
